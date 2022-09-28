1d5cbcd94c8abc48845b694a458d37ad45b416c8
Fernanda <mfalonzosalazar@gmail.com>
2022-09-27 18:43:21 -0700

    daily challenge

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let word = 'Supercalifragilisticexpialidocious';
+let number = 8;
+let bigInt = 8n;
+console.log(word, ' is ', word.length, ' letters.');
+console.log(number, typeof(number));
+console.log(bigInt, typeof(bigInt));
